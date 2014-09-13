03806e44e03a51825746bad45b5017ad8ef23356
tourn171 <tourn171@gmail.com>
2014-09-13 04:08:43 +0000

    Finish of demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  root 'users#index'
+  resources :microposts
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
